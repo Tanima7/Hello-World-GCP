3183bb341e3904ee82c56315ffaf3a9fbe0349d6
Tanima Das <139811443+Tanima7@users.noreply.github.com>
2025-08-13 10:45:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask
-app2 = Flask(__name__)
-@app2.route('/')
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
- return 'Hello World from GitHub! 뇉뇄뇅뇆뇇뇈뇊跳
+    return "Hello, Flask on GCP!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
